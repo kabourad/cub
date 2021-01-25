d18371a3c37479e3d1edfb8140afb9976ff5f028
Amine Wali alami <awali-al@e1r2p10.1337.ma>
2021-01-25 18:05:30 +0100

    experimenting with the limits on the mvement

--- srcs/execution/key_manager.c
+++ srcs/execution/key_manager.c
@@ -3,30 +3,30 @@
 int	key_press(int keycode, t_cub *cub)
 {
 	printf("%d\n", keycode);
-    if (keycode == 53)
+	if (keycode == 53)
 		cub->keys.esc = 1;
-    else if (keycode == 13)
+	else if (keycode == 13)
 		cub->keys.w_k = 1;
-    else if (keycode == 0)
+	else if (keycode == 0)
 		cub->keys.a_k = 1;
-    else if (keycode == 1)
+	else if (keycode == 1)
 		cub->keys.s_k = 1;
-    else if (keycode == 2)
+	else if (keycode == 2)
 		cub->keys.d_k = 1;
 	return (0);
 }
 
 int	key_release(int keycode, t_cub *cub)
 {
-    if (keycode == 53)
+	if (keycode == 53)
 		cub->keys.esc = 0;
-    else if (keycode == 13)
+	else if (keycode == 13)
 		cub->keys.w_k = 0;
-    else if (keycode == 0)
+	else if (keycode == 0)
 		cub->keys.a_k = 0;
-    else if (keycode == 1)
+	else if (keycode == 1)
 		cub->keys.s_k = 0;
-    else if (keycode == 2)
+	else if (keycode == 2)
 		cub->keys.d_k = 0;
 	return (0);
 }
@@ -43,18 +43,17 @@ int	key_close(t_cub *cub)
 
 int	key_manager(t_cub *cub)
 {
+	t_vec	pos;
+
+	pos = cub->parse.player.pos;
 	if (cub->keys.a_k)
 		cub->parse.player.pos.x -= 0.2;
-		// cub->parse.player.pos.x -= 10 / G_BS;
-	if (cub->keys.d_k)
+	if (cub->keys.d_k && is_valid(cub->parse.map[(int)pos.y][(int)(pos.x)])) // changes here
 		cub->parse.player.pos.x += 0.2;
-		// cub->parse.player.pos.x += 10 / G_BS;
 	if (cub->keys.w_k)
 		cub->parse.player.pos.y -= 0.2;
-		// cub->parse.player.pos.y -= 10 / G_BS;
-	if (cub->keys.s_k)
+	if (cub->keys.s_k && is_valid(cub->parse.map[(int)pos.y][(int)pos.x])) // changes here
 		cub->parse.player.pos.y += 0.2;
-		// cub->parse.player.pos.y += 10 / G_BS;
 	if (cub->keys.esc)
 		key_close(cub);
 	return (0);
