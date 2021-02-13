d63a748c2bbabd129b97a69f5b9c42147a5c7848
kabourad <58525969+kabourad@users.noreply.github.com>
2021-02-13 23:00:54 +0100

    amine is stupid but i love him

--- srcs/parsing/map_parse.c
+++ srcs/parsing/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: kabourad <kabourad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 00:20:33 by kabourad          #+#    #+#             */
-/*   Updated: 2021/02/13 22:53:08 by kabourad         ###   ########.fr       */
+/*   Updated: 2021/02/13 23:00:18 by kabourad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,7 +130,7 @@ void			map_fill(int fd, t_parse *stru)
 	stru->map = map_convert(map);
 	stru->sprites = sprite_fill(stru);
 	stru->ids |= MP_ID;
-	if (stru->ids != 512)
+	if (stru->ids != 1023)
 		quit("No player found.", NULL);
 	for (int j = 0; stru->map[j]; j++)
 	{
